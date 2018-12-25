16d29c490a6d0ffdf7fd8e4b3bb1d51431175690
Qs-F <qsf@de-liker.com>
2018-12-26 02:11:34 +0900

    Fmt

--- combination.c
+++ combination.c
@@ -14,7 +14,7 @@ typedef struct elem_t {
   struct elem_t *NEXT;
 } elem_t;
 
-typedef elem_t** table_t;
+typedef elem_t **table_t;
 
 table_t init() {
   table_t table;
@@ -31,13 +31,15 @@ table_t init() {
 
 void printb(unsigned int v) {
   unsigned int mask = (int)1 << (sizeof(v) * 4 - 1);
-  do putchar(mask & v ? '1' : '0');
+  do
+    putchar(mask & v ? '1' : '0');
   while (mask >>= 1);
   putchar('\n');
 }
 
 void printe(elem_t *e) {
-  printf("me: %p, key: %u, n: %d, PREV: %p, NEXT: %p\n", e, e->key, e->n, e->PREV, e->NEXT);
+  printf("me: %p, key: %u, n: %d, PREV: %p, NEXT: %p\n", e, e->key, e->n,
+         e->PREV, e->NEXT);
 }
 
 void print(table_t t) {
@@ -48,7 +50,7 @@ void print(table_t t) {
       e = t[i];
       while (e != NULL) {
         printe(e);
-        e = e -> NEXT;
+        e = e->NEXT;
       }
     }
   }
@@ -63,14 +65,12 @@ unsigned int hash(unsigned int key) {
 
 elem_t *search(table_t table, unsigned int key) {
   unsigned int h = hash(key);
-  if (table[h] == NULL)
-    return NULL;
+  if (table[h] == NULL) return NULL;
   elem_t *e = table[h];
-  while (e != NULL && e -> key != key) {
-    e = e -> NEXT;
+  while (e != NULL && e->key != key) {
+    e = e->NEXT;
   }
-  if (e == NULL || e -> key != key)
-    return NULL;
+  if (e == NULL || e->key != key) return NULL;
   return e;
 }
 
@@ -78,44 +78,37 @@ int ope(table_t table, unsigned int key, int n) {
   elem_t *v;
   unsigned int h = hash(key);
   if ((v = search(table, key)) != NULL) {
-    if (((v -> n) + n) == 0) {
-      if (v -> PREV != NULL)
-        v -> PREV -> NEXT = v -> NEXT;
-      if (v -> NEXT != NULL)
-        v -> NEXT -> PREV = v -> PREV;
-      if (table[h] == v)
-        table[h] = v -> NEXT;
-      v -> n = 0;
+    if (((v->n) + n) == 0) {
+      if (v->PREV != NULL) v->PREV->NEXT = v->NEXT;
+      if (v->NEXT != NULL) v->NEXT->PREV = v->PREV;
+      if (table[h] == v) table[h] = v->NEXT;
+      v->n = 0;
       free(v);
       return 0;
     }
-    v -> n += n;
-    return (v -> n);
+    v->n += n;
+    return (v->n);
   }
   elem_t *e = (elem_t *)malloc(sizeof(elem_t));
   if (e == NULL) {
     fprintf(stderr, "Cannot allocate memory for element\n");
     return 0;
   }
-  e -> key = key;
-  e -> n = n;
-  e -> PREV = NULL;
-  e -> NEXT = table[h];
-  if (e -> NEXT != NULL)
-    e -> NEXT -> PREV = e;
+  e->key = key;
+  e->n = n;
+  e->PREV = NULL;
+  e->NEXT = table[h];
+  if (e->NEXT != NULL) e->NEXT->PREV = e;
   table[h] = e;
   return n;
 }
 
-int inc(table_t table, unsigned int key) {
-  return ope(table, key, 1);
-}
+int inc(table_t table, unsigned int key) { return ope(table, key, 1); }
 
-int dec(table_t table, unsigned int key) {
-  return ope(table, key, -1);
-}
+int dec(table_t table, unsigned int key) { return ope(table, key, -1); }
 
-void factor_reduction(table_t table, unsigned int dividend, unsigned int divisor) {
+void factor_reduction(table_t table, unsigned int dividend,
+                      unsigned int divisor) {
   for (int i = 2; i <= dividend; i++) {
     while (dividend % i == 0) {
       dividend /= i;
@@ -144,8 +137,7 @@ unsigned long long int combination(unsigned int n, unsigned int r) {
   if (t == NULL) {
     return 0;
   }
-  while (r > 0)
-    factor_reduction(t, n-- ,r--);
+  while (r > 0) factor_reduction(t, n--, r--);
 
   unsigned long long int product = 1;
   elem_t *e;
@@ -153,8 +145,8 @@ unsigned long long int combination(unsigned int n, unsigned int r) {
     if (t[i] != NULL) {
       e = t[i];
       while (e != NULL) {
-        product *= ullpow(e -> key, e -> n);
-        e = e -> NEXT;
+        product *= ullpow(e->key, e->n);
+        e = e->NEXT;
       }
     }
   }
